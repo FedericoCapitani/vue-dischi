40582554240f0f52baed5c737808c3919a01838b
FedericoCapitani <federico.capitani@outlook.it>
2022-05-05 15:35:07 +0200

    creater site header

--- dischi/src/components/SiteHeader.vue
+++ dischi/src/components/SiteHeader.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="navbar">
-        <img src="@/assets/img/spotify-logo.jpg" alt="">
+        <img src="@/assets/img/logo.png" alt="">
     </div>
 </template>
 
 <style lang="scss" scoped>
     .navbar{
         background-color: $sp-card;
-        
+        padding: 1rem 2rem;
+        img{
+            width: 80px;
+        }
     }
 </style>
