4937d7ab655e2e2871ab970bc696c615e9f34968
FedericoCapitani <federico.capitani@outlook.it>
2022-05-05 15:25:01 +0200

    header

--- dischi/src/components/SiteHeader.vue
+++ dischi/src/components/SiteHeader.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="navbar">
-        
+        <img src="@/assets/img/favicon.ico" alt="">
     </div>
 </template>
+
+<style lang="scss" scoped>
+    .navbar{
+        background-color: $sp-card;
+        padding: 1rem 0;
+        .img{
+            color: greenyellow;
+        }
+    }
+</style>
