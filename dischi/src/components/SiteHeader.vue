1661e99d0e8527599d6c2130dc184c76a3e98856
FedericoCapitani <federico.capitani@outlook.it>
2022-05-05 15:32:25 +0200

    spotify logo

--- dischi/src/components/SiteHeader.vue
+++ dischi/src/components/SiteHeader.vue
@@ -1,15 +1,12 @@
 <template>
     <div class="navbar">
-        <img src="@/assets/img/favicon.ico" alt="">
+        <img src="@/assets/img/spotify-logo.jpg" alt="">
     </div>
 </template>
 
 <style lang="scss" scoped>
     .navbar{
         background-color: $sp-card;
-        padding: 1rem 0;
-        .img{
-            color: greenyellow;
-        }
+        
     }
 </style>
