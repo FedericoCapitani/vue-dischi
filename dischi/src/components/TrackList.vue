f6aa941b2d01c192a13edaef547337b81da02a97
FedericoCapitani <federico.capitani@outlook.it>
2022-05-05 16:50:16 +0200

    creare cards senza component

--- dischi/src/components/TrackList.vue
+++ dischi/src/components/TrackList.vue
@@ -2,8 +2,15 @@
     <div>
         <section class="tracks" v-if="!loading">
             <div class="container">
-                <div>
-                <Track :track="track" v-for="(track, index) in tracks" :key="index"/></div>
+                <!-- <div>
+                <Track :track="track" v-for="track in tracks" :key="track.title"/></div>
+            </div> -->
+            <div class="card" v-for="(track, index) in tracks" :key="index">
+                <img :src="track.poster" alt="" />
+                <h2 class="info">{{ track.title}}</h2>
+                <div class="info">{{ track.author }}</div>
+                <div class="info">{{ track.year }}</div>
+            </div>
             </div>
         </section>
             <div v-else class="loading">
@@ -14,12 +21,12 @@
 
 <script>
 import axios from "axios";
-import Track from "@/components/TrackComponent";
+/* import Track from "@/components/TrackComponent"; */
 export default {
-  name: "TrackListComponent",
+  /* name: "TrackListComponent",
   components: {
     Track,
-  },
+  }, */
   data() {
     return {
       API_URL: "https://flynn.boolean.careers/exercises/api/array/music",
@@ -30,9 +37,7 @@ export default {
   },
   methods: {
     callApi() {
-      axios
-        .get(this.API_URL)
-        .then((response) => {
+      axios.get(this.API_URL).then((response) => {
           this.tracks = response.data;
           this.loading = false;
         })
